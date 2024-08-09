ef3b301c46bb2aaceba78bd4f3d14a87beb3fab0
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-08-09 19:28:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
 
+document.addEventListener('DOMContentLoaded',()=>{
+	const app = document.querySelector('#app');
+	const switchBtn = document.querySelector('#swap');
+
+	function swapTheme(){
+		if(app.classList.contains('day')){
+			app.classList.remove('day');
+			app.classList.add('night');
+			switchBtn.classList.remove('button_day');
+			switchBtn.classList.add('button_night');
+		}else{
+			app.classList.remove('night');
+			app.classList.add('day');
+			switchBtn.classList.remove('button_night');
+			switchBtn.classList.add('button_day');
+		}
+	}
+	switchBtn.addEventListener('click',swapTheme);
+});
